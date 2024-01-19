9747a8a397ce7938968c6a3dc318b32b24ca2f8d
WyattZZZZ <145357227+WyattZZZZ@users.noreply.github.com>
2024-01-19 13:47:54 +0800

    Add files via upload

--- bank.py
+++ bank.py
@@ -1,28 +1,37 @@
-from product.src import *
+from product.src import deposit, loan, remittance
+import yaml
 
 
 class Bank:
     def __init__(self, interest_rate):
         self.interest_rate = interest_rate
         self.time = 0
-        self.user =
+        self.user = {}
+        self.loan_ls = {}
+        self.deposit_ls = {}
+
+    def creat_account(self, name):
+        self.user.update({name: {"deposit": False, "loan": False}})
+        with open('user/usr_ls.yaml', 'w') as file:
 
 
-    def show_total_funding(self):
-        print(self.total_funding)
+    def choose_service(self, name, service_type):
+        if service_type == "deposit":
+            self.user[name["deposit"]] = True
+            self.deposit_ls.update({name: 0})
+            with open('user/deposit_ls.yaml', 'w') as file:
+                yaml.dump(self.deposit_ls, file, default_flow_style=False)
+        elif service_type == "loan":
+            self.user[name["loan"]] = True
+            self.loan_ls.update({name: 0})
+            with open('user/loan_ls.yaml', 'w') as file:
+                yaml.dump(self.loan_ls, file, default_flow_style=False)
+        with open('user/usr_ls.yaml', 'w') as file:
+            yaml.dump(self.user, file, default_flow_style=False)
+
 
     def date_update(self, num=0):
         self.time += 1
-        for keys in self.fund:
-            value = self.fund.get(keys)
-            if num > 1:
-                self.fund[keys] = value * (1 + self.interest_rate)**num
-            else:
-                self.fund[keys] = value * (1 + self.interest_rate)
-        self.total_funding = sum(list(self.fund.values()))
-
-    def show_fund(self):
-        print(self.fund)
 
 
 
